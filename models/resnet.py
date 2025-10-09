bd3ac7a4ee676ba5368dd0f400554abcefe37bda
Dimitri <s233730@dtu.dk>
2025-10-09 12:02:45 +0200

    Update resnet implementation for FRN

--- models/resnet.py
+++ models/resnet.py
@@ -21,10 +21,15 @@ from .filter_response_norm import FilterResponseNorm
 ModuleDef = Any
 
 
+def identity(x):
+    return x
+
+
 class BasicBlock(nn.Module):
     filters: int
     norm: ModuleDef
     activation: Callable
+    use_bias: bool
     strides: tuple[int, int] = (1, 1)
 
     @nn.compact
@@ -35,17 +40,18 @@ class BasicBlock(nn.Module):
             features=self.filters,
             kernel_size=(3, 3),
             strides=self.strides,
-            use_bias=False,
+            use_bias=self.use_bias,
             padding="SAME",
         )(x)
         out = self.norm()(out)
+
         out = self.activation(out)
 
         out = nn.Conv(
             features=self.filters,
             kernel_size=(3, 3),
             strides=(1, 1),
-            use_bias=False,
+            use_bias=self.use_bias,
             padding="SAME",
         )(out)
         out = self.norm()(out)
@@ -55,7 +61,7 @@ class BasicBlock(nn.Module):
                 features=self.filters,
                 kernel_size=(1, 1),
                 strides=self.strides,
-                use_bias=False,
+                use_bias=self.use_bias,
                 padding="SAME",
                 name="conv_proj",
             )(residual)
@@ -69,12 +75,12 @@ class ResNet(nn.Module):
     block: ModuleDef
     num_classes: int
     num_filters: int
-    activation: Callable = nn.relu
+    activation: Callable
+    use_bias: bool
     dtype: Any = jnp.float32
 
     @nn.compact
     def __call__(self, x):
-
         norm = partial(FilterResponseNorm)
 
         x = nn.Conv(
@@ -82,10 +88,10 @@ class ResNet(nn.Module):
             kernel_size=(3, 3),
             strides=(1, 1),
             padding="SAME",
-            use_bias=False,
+            use_bias=self.use_bias,
         )(x)
+
         x = norm()(x)
-        x = self.activation(x)
 
         for i, block_size in enumerate(self.stage_sizes):
             for j in range(block_size):
@@ -94,6 +100,7 @@ class ResNet(nn.Module):
                     self.num_filters * (2**i),
                     strides=strides,
                     norm=norm,
+                    use_bias=self.use_bias,
                     activation=self.activation,
                 )(x)
 
@@ -103,4 +110,11 @@ class ResNet(nn.Module):
         return x
 
 
-ResNet20 = partial(ResNet, stage_sizes=(3, 3, 3), block=BasicBlock, num_filters=16)
+ResNet20 = partial(
+    ResNet,
+    stage_sizes=(3, 3, 3),
+    block=BasicBlock,
+    num_filters=16,
+    use_bias=True,
+    activation=identity,
+)
